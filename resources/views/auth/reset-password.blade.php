37d6eaa55acd37d60729f8097c05eb70a79eec6e
nabillastywn <116862851+nabillastywn@users.noreply.github.com>
2024-07-07 21:46:09 +0700

    refactor(reset-pass): refactor title and form

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,81 +1,62 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<div class="container position-sticky z-index-sticky top-0">
-    <div class="row">
-        <div class="col-12">
-            @include('layouts.navbars.guest.navbar')
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Email Verification</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+
+<body>
+    <div class="container position-sticky z-index-sticky top-0">
+        <div class="row">
+            <div class="col-12">
+                @include('layouts.navbars.guest.navbar')
+            </div>
         </div>
     </div>
-</div>
-<main class="main-content mt-0">
-    <section>
-        <div class="page-header min-vh-100">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column mx-lg-0 mx-auto">
-                        <div class="card card-plain">
-                            <div class="card-header pb-0 text-start">
-                                <h4 class="font-weight-bolder">Reset Password</h4>
-                                <p class="mb-0">Please enter your new password</p>
-                            </div>
-                            <div class="card-body">
-                                <form method="POST" action="{{ route('password.update') }}">
-                                    @csrf
-                                    <input type="hidden" name="token" value="{{ $token }}">
-                                    <div class="form-group">
-                                        <label for="email">Email Address</label>
-                                        <input id="email" type="email"
-                                            class="form-control @error('email') is-invalid @enderror" name="email"
-                                            value="{{ $email ?? old('email') }}" required autocomplete="email"
-                                            autofocus>
-                                        @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                        @enderror
+    <main class="main-content mt-0">
+        <section>
+            <div class="page-header min-vh-100">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column mx-lg-0 mx-auto">
+                            <div class="card card-plain mt-5">
+                                <div class="card-header pb-0 text-start">
+                                    <h4 class="font-weight-bolder">Email Verification</h4>
+                                </div>
+                                <div class="card-body">
+                                    @if ($status == 'success')
+                                    <div class="alert alert-success">
+                                        {{ $message }}
                                     </div>
-                                    <div class="form-group">
-                                        <label for="password">Password</label>
-                                        <input id="password" type="password"
-                                            class="form-control @error('password') is-invalid @enderror" name="password"
-                                            required autocomplete="new-password">
-                                        @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                        @enderror
+                                    @elseif ($status == 'info')
+                                    <div class="alert alert-info">
+                                        {{ $message }}
                                     </div>
-                                    <div class="form-group">
-                                        <label for="password-confirm">Confirm Password</label>
-                                        <input id="password-confirm" type="password" class="form-control"
-                                            name="password_confirmation" required autocomplete="new-password">
+                                    @elseif ($status == 'error')
+                                    <div class="alert alert-danger">
+                                        {{ $message }}
                                     </div>
-                                    <div class="form-group">
-                                        <button type="submit" class="btn btn-primary">
-                                            Reset Password
-                                        </button>
-                                    </div>
-                                </form>
-                            </div>
-                            <div id="alert">
-                                @include('components.alert')
+                                    @endif
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div
-                        class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
-                        <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden"
-                            style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signin-ill.jpg');
-                                        background-size: cover;">
-                            <span class="mask bg-gradient-primary opacity-6"></span>
-                            <h4 class="mt-5 text-white font-weight-bolder position-relative">SIARAN</h4>
-                            <p class="text-white position-relative">Sistem Layanan Pengaduan dan Pelaporan</p>
+                        <div
+                            class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
+                            <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden"
+                                style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signin-ill.jpg'); background-size: cover;">
+                                <span class="mask bg-gradient-primary opacity-6"></span>
+                                <h4 class="mt-5 text-white font-weight-bolder position-relative">SIARAN</h4>
+                                <p class="text-white position-relative">Sistem Layanan Pengaduan dan Pelaporan</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
-</main>
-@endsection
+        </section>
+    </main>
+</body>
+
+</html>
